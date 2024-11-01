fbb4b217ae4243b5ffa2af52f18ad134f5cdc732
Jamie Hurst <jamie@jamiehurst.co.uk>
2024-11-01 10:22:14 +0000

    Adding settings basic logic tests, no onchange

--- src/__mocks__/obsidian.ts
+++ src/__mocks__/obsidian.ts
@@ -6,8 +6,50 @@ export class TFile extends TAbstractFile {
   public basename!: string;
   public name!: string;
 }
+class BaseComponent {
+  onChange(cb: (val: any) => any) {
+    return this;
+  }
+  setDisabled(b: boolean) {
+    return this;
+  }
+  setValue(s: string) {
+    return this;
+  }
+}
+export class TextComponent extends BaseComponent {}
+export class ToggleComponent extends BaseComponent {}
 
 export const moment = momentImpl;
 
 // Mock the Notice class so it can be checked
 export const Notice = jest.fn();
+
+// Mock the Setting class
+export class Setting {
+  constructor(el: HTMLElement) {
+    return this;
+  }
+
+  setDesc(s: string) {
+    return this;
+  }
+
+  setHeading(s: string) {
+    return this;
+  }
+
+  setName(s: string) {
+    return this;
+  }
+
+  addText(cb: (text: TextComponent) => any) {
+    cb(new TextComponent);
+    return this;
+  }
+
+  addToggle(cb: (toggle: ToggleComponent) => any) {
+    cb(new ToggleComponent);
+    return this;
+  }
+}
